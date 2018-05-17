7236109588aad9a28064da8d69e5dd37649cae1f
SergeyShekera <SergeyShekera98@gmail.com>
2018-05-17 23:09:18 +0300

    removed unnecessary

--- shop/urls.py
+++ shop/urls.py
@@ -1,17 +1,5 @@
 from django.urls import path
 from shop.views import ProductList, ProductDetail
-# from django.contrib.auth.decorators import login_required
-#
-# urlpatterns = [
-#
-#     path('all', views.ProductList, name='ProductListByCategory'),
-#     path('add', add_stud),
-#     path('attadd', attestat_add),
-#     path('attall', all_att),
-#     path('find', find_students)
-#
-# ]
-from . import views
 
 app_name='shop'
 
@@ -21,8 +9,3 @@ urlpatterns = [
     path('<int:id>/<slug:slug>/', ProductDetail, name='ProductDetail' )
 ]
 
-# urlpatterns = [
-#     path('all', all_groups),
-#     path('add', add_group),
-#     path('<int:id>', GetGroup.as_view())
-# ]
