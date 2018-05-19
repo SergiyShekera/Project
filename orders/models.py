baf0140aa7edd49781ebe635b9dcccd3de387c4d
SergeyShekera <SergeyShekera98@gmail.com>
2018-05-19 16:17:14 +0300

    Activation of the coupon in the order

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from shop.models import Product
-
+from cupons.models import Cupon
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Order(models.Model):
     first_name = models.CharField(verbose_name='Имя', max_length=50)
@@ -12,6 +14,8 @@ class Order(models.Model):
     created = models.DateTimeField(verbose_name='Создан', auto_now_add=True)
     updated = models.DateTimeField(verbose_name='Обновлен', auto_now=True)
     paid = models.BooleanField(verbose_name='Оплачен', default=False)
+    cupon = models.ForeignKey(Cupon, on_delete=models.CASCADE, related_name='orders', null=True, blank=True)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created', )
@@ -22,7 +26,8 @@ class Order(models.Model):
         return 'Заказ: {}'.format(self.id)
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
